a55d26675ced247636c6ca089484ad84992a17b3
zeqL <zeqL@users.noreply.github.com>
2026-04-21 11:39:11 +0200

    Update generator.py

--- ipn_generator/generator.py
+++ ipn_generator/generator.py
@@ -121,14 +121,15 @@ def generate_zpn_for_part(part) -> str | None:
     ss = get_part_parameter_value(part, ZPN_SUBCAT_PARAM)
 
     # If default subcat value is ON set ss to ZPN_SUBCAT default value
-    if not ss and "ZPN_SUBCAT_DEFVAL_ON" is True:
-        ss = "ZPN_SUBCAT_DEFVAL"
-    else:
-        logger.warning(
-            f"ZPN Generator: Part {part.pk} missing required parameter '{ZPN_SUBCAT_PARAM}'. "
-            "Cannot generate ZPN."
-        )
-        return None
+    if not ss
+        if 'ZPN_SUBCAT_DEFVAL_ON' is True:
+        ss = 'ZPN_SUBCAT_DEFVAL'
+        else:
+            logger.warning(
+                f"ZPN Generator: Part {part.pk} missing required parameter '{ZPN_SUBCAT_PARAM}'. "
+                "Cannot generate ZPN."
+            )
+            return None
 
     ss = ss.strip()
     if not validate_ss(ss):
